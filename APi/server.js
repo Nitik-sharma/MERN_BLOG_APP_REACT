9ee3ffacd855d9c643ee351cde0327ca023cf6b0
Nitik sharma <sharmanitik36@gmail.com>
2024-01-27 22:24:22 +0530

    Make schema and connect mongoDb

--- APi/server.js
+++ APi/server.js
@@ -1,10 +1,23 @@
 import express from "express";
+import mongoose from "mongoose";
+import dotenv from "dotenv";
+
+dotenv.config();
+
+// contant------->
+const MONGO_URI = process.env.MONGO_URI;
+const PORT = process.env.PORT;
+
+mongoose
+  .connect(MONGO_URI)
+  .then(() => console.log("MongoDB is Connect Sucessfully"))
+  .catch((err) => console.log(err));
 
 const app = express();
 app.get("/", (req, res) => {
   res.send("Server is running ");
 });
 
-app.listen(4000, () => {
+app.listen(PORT, () => {
   console.log("Server is running on 4000 ");
 });
