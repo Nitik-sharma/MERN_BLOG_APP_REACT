683722c90bcc5a849a488ea11816b2442e840e92
Nitik sharma <sharmanitik36@gmail.com>
2024-02-03 21:33:11 +0530

    Add presist toolkit

--- client/src/redux/store.js
+++ client/src/redux/store.js
@@ -1,7 +1,26 @@
-import { configureStore } from "@reduxjs/toolkit";
+import { configureStore, combineReducers } from "@reduxjs/toolkit";
 import userReducers from "../redux/user/userSlice";
+import { persistReducer } from "redux-persist";
+import storage from "redux-persist/lib/storage";
+import persistStore from "redux-persist/es/persistStore";
+
+const rootReducers = combineReducers({
+  user: userReducers,
+});
+const configPresist = {
+  key: "root",
+  storage,
+  vaersion: 1,
+};
+const persistedReducer = persistReducer(configPresist, rootReducers);
+
 export const store = configureStore({
-  reducer: {
-    user: userReducers,
+  reducer: persistedReducer,
+  middleware: (getDefaultMiddleware) => {
+    return getDefaultMiddleware({
+      serializableCheck: false,
+    });
   },
 });
+
+export const persistor = persistStore(store);
